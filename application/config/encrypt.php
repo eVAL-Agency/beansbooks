bc583f061bbbf27fe3c5ca503e5516482b20bc62
Charlie Powell <charlie@evalagency.com>
2016-05-17 09:10:03 -0400

    Remove some test config files, they shouldn't have been in git to begin with.

--- application/config/encrypt.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct access allowed.');
-
- return ['default' => array (
-  'key' => '3pm2Q69sXfyVw4soA4MetzGntBUTg7RQMnnhvCB55uL6itX82fIihNny8g8KvhbA',
-  'cipher' => 'rijndael-128',
-  'mode' => 'nofb',
-)];
